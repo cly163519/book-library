d6474ee4076bc90a8964a40c223d65c2ff54c08e
Laura <laurainnz2@gmail.com>
2025-03-17 10:33:28 +1300

    update 3.17

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',  // 禁用这个规则
+  },
+};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 忽略构建时的 ESLint 错误
+  },
 };
 
 export default nextConfig;
